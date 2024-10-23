5009d1a393452a453b9924f60a399627993ae41b
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-10-23 21:48:48 +0530

    test

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { sequelize, connectToMySQL } = require("./connect");
-const urlRoute = require("./routes/url");
+const { sequelize, connectToMySQL } = require("./Backend/connect");
+const urlRoute = require("./Backend/routes/url");
 const app = express();
 const path = require("path");
-const URL = require("./models/url");
+const URL = require("./Backend/models/url");
 require("dotenv").config();
 // Define the port (either from environment variable or default to 8080)
 const PORT = process.env.PORT || 8080; // Using environment variable if set, else default to 8080
